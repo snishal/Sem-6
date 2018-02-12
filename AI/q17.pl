b1a1213b814877248d0f15ca8e4b2352efb5cfef
Sahil <snishal33@gmail.com>
2018-02-13 03:29:09 +0530

    Ques17 AI

--- AI/q17.pl
+++ AI/q17.pl
@@ -4,7 +4,7 @@ memberOf(X, [_|Y]) :- memberOf(X, Y).
 conc([], L2, L2).
 conc([X|L1], L2, [X|L3]) :- conc(L1, L2, L3).
 
-remove_dup([],_) :- !.
+remove_dup([],[]) :- !.
 remove_dup([H|T], R) :- memberOf(H,T), remove_dup(T,R).
 remove_dup([H|T], [H|T1]) :- \+(memberOf(H,T)),remove_dup(T,T1).
 
