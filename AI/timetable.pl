2498596db540f99ee4a647ea9147b43a4e377d2e
Unknown <snishal33@gmail.com>
2018-03-27 17:44:37 +0530

    Auto stash before merge of "master" and "origin/master"

--- AI/timetable.pl
+++ AI/timetable.pl
@@ -1,5 +1,6 @@
 :-dynamic(roomAvailable/2).
-:-dynamic(roomAlloted/2).
+:-dynamic(roomAlloted/3).
+:-dynamic(demand/2).
 
 roomAvailable(401,9).
 roomAvailable(401,11).
@@ -7,14 +8,27 @@ roomAvailable(401,11).
 roomAvailable(402,9).
 roomAvailable(402,11).
 
-demand('A', 9).
-demand('B', 9).
-demand('C', 9).
-demand('D', 9).
+inputloop(0).
+inputloop(I) :- I>0,
+	       write('Name of the teacher: '), read(Name),
+	       write(' Time: '), read(Time), nl,
+	       assertz(demand(Name,Time)),
+	       N is I-1, inputloop(N).
 
-allot([]).
-allot([[X,Y]|T]) :- roomAvailable(Z,Y), retract(roomAvailable(Z,Y)), asserta(roomAlloted(X,Y,Z)), allot(T); roomAvailable(_,_), allot([[X|_]|T]).
+allot([], CRemain, CRemain).
+allot([[Name,Time]|Tail], CRemain, FRemain) :- roomAvailable(Room,Time), retract(roomAvailable(Room,Time)), assertz(roomAlloted(Name,Room,Time)), allot(Tail, CRemain, FRemain).
+allot([[Name,_]|Tail], CRemain, FRemain) :- append(CRemain, [[Name, _]], NCRemain), allot(Tail, NCRemain, FRemain).
 
-demands(L) :- findall([X,Y], demand(X,Y), L).
+demands(Demands) :- findall([Name,Time], demand(Name,Time), Demands).
 
-schedule :- demands(L), allot(L), findall([X,Y,Z], roomAlloted(X,Y,Z), S), write(S), !.
+output([]).
+output([[Name,Time,Room]|Tail]) :- format('Teacher: ~w Time: ~w Room:  ~w', [Name,Time,Room]), nl, output(Tail).
+
+schedule :- write('Enter the number of demands: '), read(D), inputloop(D),
+	    demands(L),
+	    allot(L, [], Remaining),
+	    allot(Remaining, [], []),
+	    findall([X,Y,Z], roomAlloted(X,Y,Z), S),
+	    output(S), retractall(roomAlloted/3), !; write('Can\'t be Scheduled.'), nl, !.
+
+:- initialization(schedule).
