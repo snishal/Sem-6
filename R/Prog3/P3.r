5598f30b7d2f5bb06946d16cb1adbf465b765c56
Sahil <snishal33@gmail.com>
2018-04-03 21:26:00 +0530

    Update

--- R/Prog3/P3.r
+++ R/Prog3/P3.r
@@ -2,6 +2,8 @@ library(BBmisc)
 
 wine = read.csv(file='wine.csv', header = TRUE)
 
+colMeanWine = c()
+
 for(col in names(wine)){
   if(class(wine[,col]) == 'numeric' || class(wine[,col]) == 'integer'){
     colMeanWine[col] = colMeans(wine[col], na.rm = TRUE)
@@ -21,22 +23,22 @@ for(col in names(wine)){
 View(wine)
 View(colMeanWine)
 
-dirty_iris = read.csv(file='dirty_iris.csv', header = TRUE)
+colMeanIris = c()
 
-for(col in names(dirty_iris)){
-  if(class(dirty_iris[,col]) == 'numeric' || class(dirty_iris[,col]) == 'integer'){
-    colMeanIris[col] = colMeans(dirty_iris[col], na.rm = TRUE)
+for(col in names(iris)){
+  if(class(iris[,col]) == 'numeric' || class(iris[,col]) == 'integer'){
+    colMeanIris[col] = colMeans(iris[col], na.rm = TRUE)
     if(colMeanIris[col] != 0){
-      dirty_iris[col] = normalize(dirty_iris[col], method = "standardize")
+      iris[col] = normalize(iris[col], method = "standardize")
     }
   }
 }
 
-for(col in names(dirty_iris)){
-  if(class(dirty_iris[,col]) == 'numeric' || class(dirty_iris[,col]) == 'integer'){
-    colMeanIris[col] = colMeans(dirty_iris[col], na.rm = TRUE)
+for(col in names(iris)){
+  if(class(iris[,col]) == 'numeric' || class(iris[,col]) == 'integer'){
+    colMeanIris[col] = colMeans(iris[col], na.rm = TRUE)
   }
 }
 
-View(dirty_iris)
+View(iris)
 View(colMeanIris)
