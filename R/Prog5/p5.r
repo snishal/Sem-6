72f746a9a3244a252de2bfdf9811aa65cbdf2ab4
Sahil <snishal33@gmail.com>
2018-04-04 00:10:17 +0530

    R Prog 5

--- R/Prog5/p5.r
+++ R/Prog5/p5.r
@@ -1,33 +1,65 @@
-library(caret)
-library(e1071)
-data(iris)
+library(rpart) #Decison Tree
+library(caret) #Confusion Matrix, train
+library(e1071) #Naive Bayes
+library(class) #Knn
+data(iris) #load dataset
 
 #Holdout
-## 75% of the sample size
 smp_size <- floor(0.75 * nrow(iris))
 
 train <- iris[1:smp_size, ]
 test <- iris[-(1:smp_size), ]
 
-m <- naiveBayes(Species ~ ., data = iris)
-print(m)
+#Naive Bayes
+model <- naiveBayes(Species ~ ., data = train)
+prediction <- predict(model, test)
+View(confusionMatrix(prediction, test[,5])$table, 'NaiveBayes Holdout')
 
-prediction <- predict(m, test)
-print(confusionMatrix(prediction, test[,5]))
+#Decision Tree
+model <- rpart(Species ~ ., data = train)
+prediction <- predict(model, test, type = "class")
+View(confusionMatrix(prediction, test[,5])$table, 'Decision Tree Holdout')
+
+#KNN
+prediction = knn(train[,-5], test[,-5], factor(train[,5]), k = 10)
+View(confusionMatrix(prediction, test[,5])$table, 'KNN Holdout')
 
 #Random Subsampling
-## 75% of the sample size
 smp_size <- floor(0.75 * nrow(iris))
-
-## set the seed to make your partition reproductible
 set.seed(123)
 train_ind <- sample(nrow(iris), size = smp_size)
 
 train <- iris[train_ind, ]
 test <- iris[-train_ind, ]
 
-m <- naiveBayes(iris[,-5], iris[,5])
-print(m)
+#Naive Bayes
+model <- naiveBayes(Species ~ ., data = train)
+prediction <- predict(model, test)
+View(confusionMatrix(prediction, test[,5])$table, 'NaiveBayes Random Sampling')
 
-prediction <- predict(m, test)
-print(confusionMatrix(prediction, test[,5]))
+#Decision Tree
+model <- rpart(Species ~ ., data = train)
+prediction <- predict(model, test, type = "class")
+View(confusionMatrix(prediction, test[,5])$table, 'Decision Tree Random Sampling')
+
+#KNN
+prediction = knn(train[,-5], test[,-5], factor(train[,5]), k = 10)
+View(confusionMatrix(prediction, test[,5])$table, 'KNN Random Sampling')
+
+#K-Fold Cross Validation Naive Bayes
+train_control <- trainControl(method="cv", number=10)
+model <- train(Species~., data=iris, trControl=train_control, method="nb")
+prediction <- predict(model, test)
+View(confusionMatrix(prediction, test[,5])$table, 'NaiveBayes KFOLD')
+
+#K-Fold Cross Validation Decision Tree
+train_control <- trainControl(method="cv", number=10)
+model <- train(Species~., data=iris, trControl=train_control, method="rpart")
+prediction <- predict(model, test)
+View(confusionMatrix(prediction, test[,5])$table, 'Decision Tree KFOLD')
+
+#K-Fold Cross Validation KNN
+train_control <- trainControl(method="cv", number=10)
+model <- train(Species~., data=iris, trControl=train_control, method="knn")
+prediction <- predict(model, test)
+View(confusionMatrix(prediction, test[,5])$table, 'KNN KFOLD')
