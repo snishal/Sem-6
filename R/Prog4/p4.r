63b37b2a8a0fe1444109d9939450013589f63150
Sahil <snishal33@gmail.com>
2018-04-16 02:27:39 +0530

    DM DONE

--- R/Prog4/p4.r
+++ R/Prog4/p4.r
@@ -1,2 +1,6 @@
-x = read.csv(file="https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/groceries.csv", header= FALSE)
-x
+library(arules)
+grocery = read.csv(file="grocery.csv", header = FALSE)
+
+rules <- apriori(Adult, parameter = list(supp = 0.5, conf = .5, target = "rules"))
+summary(rules)
+inspect(head(rules))
